8d132e8dca6eccd8e24eb3e795c1be21403fca56
shashwatdwi176 <dwivedishashwat176@gmail.com>
2023-10-21 18:29:07 +0530

    Added Post Detail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // App.js
 import React from 'react';
-import HomeScreen from './components/homescreen.jsx';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import HomeScreen from './components/homescreen.jsx';
+import PostScreen from './components/postscreen.jsx';
 
 const App = () => {
   return (
-   
-       <>
-        <HomeScreen />;
-       </>
-        
-     
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/post/:id" element={<PostScreen />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/postscreen.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './style.css';
+
+const PostScreen = () => {
+  // Get the post ID from the route parameters
+  const { id } = useParams();
+
+  // State to hold the post data
+  const [post, setPost] = useState(null);
+
+  // Fetch post data based on the ID when the component mounts
+  useEffect(() => {
+    // API call to fetch post data
+    const fetchPost = async () => {
+      try {
+        const response = await fetch(`http://hn.algolia.com/api/v1/items/${id}`);
+        const data = await response.json();
+        setPost(data);
+      } catch (error) {
+        console.error('Error fetching post:', error);
+      }
+    };
+
+    // Call the fetchPost function
+    fetchPost();
+  }, [id]);
+
+  // Render the component
+  return (
+    <div className="container">
+      <h2 className="title">Post Detail</h2>
+      {post ? (
+        <div>
+          <h3 className="post-title">{post.title}</h3>
+          <p className="points">Points: {post.points}</p>
+
+          <h4 className="comments-heading">Comments:</h4>
+          <ul className="comments-list">
+            {post.children &&
+              post.children.map((comment) => (
+                <li key={comment.id} className="comment">
+                  {/* Display the comment text */}
+                  <blockquote
+                    className="comment-box"
+                    dangerouslySetInnerHTML={{ __html: comment.text }}
+                  />
+                </li>
+              ))}
+          </ul>
+        </div>
+      ) : (
+        // Show a loading message while fetching data
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+// Export the PostScreen component
+export default PostScreen;
